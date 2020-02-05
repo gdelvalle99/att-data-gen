44c31ad0e65852a05ed6fdcf721f7392511cb2a8
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2020-02-05 08:21:28 -0800

    Added dlib facial landmark detection

---
+++ src/landmark_detection.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import pandas as pd
+import os
+import dlib
+
+def rect_to_bb(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+
+    return (x,y,w,h)
+
+def shape_to_np(shape, dtype="int"):
+
+	coords = np.zeros((68, 2), dtype=dtype)
+
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+
+
+	return coords
+
+
+
+def find_facial_landmarks(img):
+    p = "shape_predictor_68_face_landmarks.dat"
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(p)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    rects = detector(img_gray,1)
+
+    for (i,rect) in enumerate(rects):
+        shape = predictor(img_gray, rect)
+        shape = shape_to_np(shape)
+        (x,y,w,h) = rect_to_bb(rect)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0))
+
+        for(x,y) in shape:
+            cv2.circle(img, (x,y), 1, (0,0,255),-1)
+
+    cv2.imshow("Output", img)
+    cv2.waitKey(0)
+    return
+
+img = cv2.imread("000001.jpg")
+find_facial_landmarks(img)
