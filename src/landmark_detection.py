04ee31d3401b54d877216aa52029c04ece38767a
root <root@mworks03.rd.unr.edu>
2020-03-26 22:41:14 -0700

    the bash file works

--- src/landmark_detection.py
+++ src/landmark_detection.py
@@ -239,7 +239,7 @@ def process_directory_openface(dir, csv_file, dict):
 dict = use_bbox('list_bbox_celeba.csv')
 #print(dict['000001.jpg'])
 csv_file = 'test.csv'
-path = '/home/guillermodelvalle/img-celeba/'
+path = '/home/guillermodelvalle/img_celeba'
 OpenFaceBashCommand = '/OpenFace/build/bin/FaceLandmarkImg -2Dfp -wild -fdir '+path+' -out_dir ../OpenFace_landmarks/'
 #print(OpenFaceBashCommand)
 #process_directory(path, csv_file, dict)
