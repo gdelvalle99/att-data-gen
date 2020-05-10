22cb47951b1d5e9a07fc2108f73703f8049d84f6
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2020-05-10 13:04:59 -0700

    updated point

--- src/point.py
+++ src/point.py
@@ -1,8 +1,27 @@
 import cv2
 import dlib
+import numpy as np
+
+def rect_to_bb(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+
+    return (x,y,w,h)
+
+#imported from face utils
+def shape_to_np(shape, dtype="int"):
+
+	coords = np.zeros((68, 2), dtype=dtype)
+
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+
+	return coords
 
 detector = dlib.get_frontal_face_detector()
-img = cv2.imread("004151.jpg")
+img = cv2.imread("030481.jpg")
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 rects = detector(img_gray,1)
